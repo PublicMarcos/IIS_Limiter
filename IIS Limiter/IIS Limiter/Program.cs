b2dc14918c4c9218002ef13456b8980e0f3dac34
Marcos <PublicMarcos@web.de>
2016-12-10 17:10:29 +0100

    #1

---
+++ IIS Limiter/IIS Limiter/Program.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace IIS_Limiter
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string ConfigPath = string.Empty, LogPath = string.Empty;
+            var CheckInterval = TimeSpan.Parse("01:00:00");
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("-config=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ConfigPath = args[i].Split(new string[1] { "-config=" }, StringSplitOptions.None)[1];
+                }
+                else if (args[i].StartsWith("-log=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    LogPath = args[i].Split(new string[1] { "-log=" }, StringSplitOptions.None)[1];
+                }
+                else if (args[i].StartsWith("-checkinterval=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    TimeSpan.TryParse(args[i].Split(new string[1] { "-checkinterval=" }, StringSplitOptions.None)[1], out CheckInterval);
+                }
+            }
+            if (string.IsNullOrWhiteSpace(ConfigPath) || string.IsNullOrWhiteSpace(LogPath))
+            {
+                Console.WriteLine("Falsche Startparameter");
+                Environment.Exit(87);
+            }
+            else
+            {
+                MainConfig = new Config(ConfigPath, LogPath);
+            }
+
+            while (true)
+            {
+                for (int i = 0; i < MainConfig.Items.Count; i++)
+                {
+                    var CurSpeed = GetAdapterSpeed(MainConfig.Items[i].Adaptername);
+                    if (MainConfig.Items[i].LastAdapterSpeed != CurSpeed)
+                    {
+                        if ((CurSpeed >= MainConfig.Items[i].SpeedTriggerFrom) && (CurSpeed <= MainConfig.Items[i].SpeedTriggerTo))
+                        {
+                            if (LimitIISSpeed(MainConfig.Items[i].WebSiteName, MainConfig.Items[i].MaxBytesPerSec, MainConfig.Items[i].MaxConn, MainConfig.Items[i].Timeout, MainConfig.Items[i].IISServiceName))
+                            {
+                                ErrorReport(string.Format("Datum: {0:G}", DateTime.Now) + " - Änderungen wurden vorgenommen");
+                            }
+                            else
+                            {
+                                ErrorReport(string.Format("Datum: {0:G}", DateTime.Now) + " - Unbekannter Fehler");
+                            }
+                        }
+                    }
+                    MainConfig.Items[i].LastAdapterSpeed = CurSpeed;
+                }
+                Thread.Sleep((int)CheckInterval.TotalMilliseconds);
+            }
+        }
+
+        private static Config MainConfig { get; set; }
+
+        private static void ErrorReport(string Message)
+        {
+            Console.WriteLine(Message);
+            File.AppendAllLines(MainConfig.LogPath, new string[1] { Message });
+        }
+
+        private static int GetAdapterSpeed(string Adaptername)
+        {
+            // The enum value of `AF_INET` will select only IPv4 adapters.
+            // You can change this to `AF_INET6` for IPv6 likewise
+            // And `AF_UNSPEC` for either one
+            foreach (IPIntertop.IP_ADAPTER_ADDRESSES net in IPIntertop.GetIPAdapters(IPIntertop.FAMILY.AF_UNSPEC))
+            {
+                if (net.FriendlyName.ToLowerInvariant().Contains(Adaptername.ToLowerInvariant()))
+                {
+                    return (int)(net.TrasmitLinkSpeed / 1000000);
+                }
+            }
+            ErrorReport("Adapter '" + Adaptername + "' nicht gefunden");
+            return 0;
+        }
+
+        private static bool LimitIISSpeed(string WebSiteName, int MaxBytesPerSec, int MaxConn, TimeSpan Timeout, string IISServiceName)
+        {
+            try
+            {
+                using (var serverManager = new ServerManager())
+                {
+                    var config = serverManager.GetApplicationHostConfiguration();
+                    var sitesSection = config.GetSection("system.applicationHost/sites");
+                    var sitesCollection = sitesSection.GetCollection();
+
+                    var siteElement = FindElement(sitesCollection, "site", "name", WebSiteName);
+                    if (ReferenceEquals(siteElement, null))
+                    {
+                        ErrorReport("Seite '" + WebSiteName + "' nicht gefunden");
+                        return false;
+                    }
+
+                    var limitsElement = siteElement.GetChildElement("limits");
+                    limitsElement["maxBandwidth"] = MaxBytesPerSec;
+                    limitsElement["maxConnections"] = MaxConn;
+                    limitsElement["connectionTimeout"] = Timeout;
+
+                    serverManager.CommitChanges();
+                    var service = new ServiceController(IISServiceName);
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.Parse("00:01:00"));
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.Parse("00:01:00"));
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static ConfigurationElement FindElement(ConfigurationElementCollection collection, string elementTagName, params string[] keyValues)
+        {
+            foreach (ConfigurationElement element in collection)
+            {
+                if (element.ElementTagName.Equals(elementTagName, StringComparison.OrdinalIgnoreCase))
+                {
+                    bool matches = true;
+                    for (int i = 0; i < keyValues.Length; i += 2)
+                    {
+                        object CurAttributeValue = element.GetAttributeValue(keyValues[i]);
+                        string value = null;
+                        if (!ReferenceEquals(CurAttributeValue, null))
+                        {
+                            value = CurAttributeValue.ToString();
+                        }
+                        if (!value.Equals(keyValues[i + 1], StringComparison.OrdinalIgnoreCase))
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+                    if (matches)
+                    {
+                        return element;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    internal sealed class Config
+    {
+        internal Config(string ConfigPath, string LogPath)
+        {
+            this.LogPath = LogPath;
+            Items = new List<ConfigItem>();
+            var RawData = File.ReadAllLines(ConfigPath);
+            string Adaptername, WebSiteName, IISServiceName;
+            int SpeedTriggerFrom = 0, SpeedTriggerTo = 0, MaxBytesPerSec = 0, MaxConn = 0;
+            TimeSpan Timeout;
+            for (int i = 0; i < RawData.Length; i++)
+            {
+                var RAWItems = RawData[i].Split(',');
+                if ((RAWItems.Length > 6) && (RAWItems[1].Contains("-")))
+                {
+                    Adaptername = RAWItems[0];
+                    WebSiteName = RAWItems[2];
+                    IISServiceName = RAWItems[6];
+                    var RAWTriggerPoints = RAWItems[1].Split('-');
+                    if (int.TryParse(RAWTriggerPoints[0], out SpeedTriggerFrom) && int.TryParse(RAWTriggerPoints[1], out SpeedTriggerTo) && int.TryParse(RAWItems[3], out MaxBytesPerSec) && int.TryParse(RAWItems[4], out MaxConn) && TimeSpan.TryParse(RAWItems[5], out Timeout))
+                    {
+                        Items.Add(new ConfigItem(Adaptername, SpeedTriggerFrom, SpeedTriggerTo, WebSiteName, MaxBytesPerSec, MaxConn, Timeout, IISServiceName));
+                    }
+                }
+            }
+        }
+        internal List<ConfigItem> Items { get; set; }
+        internal string LogPath { get; set; }
+    }
+
+    internal sealed class ConfigItem
+    {
+        internal ConfigItem(string Adaptername, int SpeedTriggerFrom, int SpeedTriggerTo, string WebSiteName, int MaxBytesPerSec, int MaxConn, TimeSpan Timeout, string IISServiceName)
+        {
+            this.Adaptername = Adaptername;
+            this.SpeedTriggerFrom = SpeedTriggerFrom;
+            this.SpeedTriggerTo = SpeedTriggerTo;
+            this.WebSiteName = WebSiteName;
+            this.MaxBytesPerSec = MaxBytesPerSec;
+            this.MaxConn = MaxConn;
+            this.Timeout = Timeout;
+            this.IISServiceName = IISServiceName;
+            this.LastAdapterSpeed = 0;
+        }
+
+        internal string Adaptername { get; set; }
+        internal int SpeedTriggerFrom { get; set; }
+        internal int SpeedTriggerTo { get; set; }
+        internal string WebSiteName { get; set; }
+        internal int MaxBytesPerSec { get; set; }
+        internal int MaxConn { get; set; }
+        internal TimeSpan Timeout { get; set; }
+        internal string IISServiceName { get; set; }
+        internal int LastAdapterSpeed { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# NEF aus JPG's filtern
+<p align="center">
+  <img src="Screenshot.png">
+</p>
+
+##Startparameter
+###-config="Path"
+(Plicht)Hier muss der Pfad zur Configdatei angegeben werden.
+###-log="Path"
+(Plicht)Hier muss der Pfad zur zukünftigen Log Datei angegeben werden.
+###-checkinterval=00:00:00
+(Optional)Hier muss man angeben in welchen Intervall das Tool die Bandbreite überprüfen soll. 1 Stunde (01:00:00) ist standard.
+
+##Configeinträge (Beispiel)
+1.Zeile: Ethernet 3,1-100,Default Web Site,52428800,150000,00:02:00,W3SVC
+2.Zeile: Ethernet 3,101-1000,Default Web Site,943718400,150000,00:01:00,W3SVC
+
+1. Adaptername = Ethernet 3
+2. Trigger von/bis in Mbit/s = 1-100
+3. Webseite, die limitiert werden soll = Default Web Site
+4. Limitierung der Bandbreite in Bytes/s = 52428800
+5. Limitierung der MaxConnections = 150000
+6. Änderung des Timeouts = 00:02:00
+7. IIS Dienstname = W3SVC
+
+Im oben angegebenen Beispiel wird die Webseite "Default Web Site" auf 50Mbit/s breschränkt, wenn der Adapter "Ethernet 3" auf 100Mbit/s oder weniger fällt. Geht die Bandbreite des Adapters auf 1Gbit/s zurück, dann kriegt die Webseite wieder 900Mbit/s.
+
+##Inhalt
+Hier finden Sie den Sourcecode für das Programm.
+
+##Zweck
+Kennen Sie das, wenn Ihr IIS Webserver zu viel Traffic verursacht, weil das, was Sie anbieten, so beliebt ist, und der Server-Provider Ihre Leitung von 1Gb/s auf 100Mb/s oder weniger limitiert?
+
+Das wäre ja kein Problem, wenn der Webserverdienst nicht auch die letzten Bits für sich beanspruchen würde, sodass man nicht einmal mehr vernünftig mit Remote Desktop auf diesen Server arbeiten kann. In solchen Fällen ist es dann notwendig, dass man manuel in den IIS Einstellung mit großer Mühe (da der Remote Desktop extrem hängt) die Bandbreite für die IIS Webseite heruntersetzt.
+
+Da die Beschränkung aufseiten des Server-Provider's aber auch Montags um 3 Uhr morgens automatisch eintreten kann, kriegt man es oft nicht mit, wann es passiert.
+
+Und genau dafür ist dieses Programm. Es überprüft die dem angegebenen Netzwerkadapter zur Verfügung stehende Bandbreite in einem festgelegten Intervall und Beschränkt automatisch bei einer Veränderung die Bandbreite der einzelnen Webseiten nach festgelegten Mustern. So muss man sich nie weider Sorgen machen, dass man nicht mehr per Remote Desktop auf dem Server kommt, weil der IIS auch die letzten Bytes für sich beansprucht. Doch nicht nur für Remote Desktop kann es verheerend sein, auch andere Dienste könnten benötigt werden.
